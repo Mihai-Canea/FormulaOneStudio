8eb467bf54052c9f87178119b6111a87307d165e
Mihai-Canea <51034102+Mihai-Canea@users.noreply.github.com>
2020-02-27 11:53:50 +0100

    UserControl added - Driver card/grid

---
+++ FormulaOneStudioSolution/FormulaOneStudioWPF/Controls/CardDriver.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormulaOneStudioWPF.Controls
+{
+    /// <summary>
+    /// Logica di interazione per CardDriver.xaml
+    /// </summary>
+    public partial class CardDriver : UserControl
+    {
+        public CardDriver()
+        {
+            InitializeComponent();
+        }
+    }
+}
