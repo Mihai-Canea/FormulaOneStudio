7302fd75fbbd7719c80602b16c834e32474dc7ee
Mihai-Canea <51034102+Mihai-Canea@users.noreply.github.com>
2020-02-27 21:35:16 +0100

    SQL automation with Python

---
+++ FormulaOneSQLCreator/main.py
@@ -0,0 +1,36 @@
+import getpass
+import json
+
+with open('../FormulaOneScrapy/drivers.json') as data_file:
+    data = json.load(data_file)
+sqlQuery = """
+INSERT INTO [dbo].[Drivers]
+(
+    Name,
+    Team,
+    Country,
+    Podlumns,
+    Points,
+    GrandsPrixEntered,
+    WorldChampionships,
+    HighestRaceFinish,
+    HighestGridPosition,
+    DateOfBirth,
+    PlaceOfBirth,
+)
+VALUES
+"""
+for drivers in data:
+    sqlQuery+="("
+    for items in drivers:
+        sqlQuery += '`"%s"`,'%(drivers[items])
+    sqlQuery+=")\n"
+
+text_file = open("Output/Output.txt", "w")
+text_file.write(sqlQuery)
+text_file.close()
+
+sqlFile= open("C:/Users/"+ getpass.getuser() +"/Documents/MSSQLDatabase/FormulaOne/Drivers.sql", "w")
+sqlFile.write(sqlQuery)
+sqlFile.close()
+print(sqlQuery)
